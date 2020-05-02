39344d2354ca0ab5e8b8bfdc2c7920a0094d4a28
Thomas Kriechbaumer <thomas@kriechbaumer.name>
2020-05-02 19:09:52 +0200

    build++

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+git push
 git push --tags origin master
 
 TAG=$(git describe --abbrev=0 --tags)
 git archive ${TAG} --prefix=DuetRRFPlugin/ --format=zip -o ~/Downloads/DuetRRFPlugin_${TAG}.zip
+
+open https://github.com/Kriechi/Cura-DuetRRFPlugin/releases/new
+open https://contribute.ultimaker.com/app/developer/plugins
